dec331c4e357d452929caa10f23a86d66d5583ca
Mikhail Kolkov <kolkov.michael2014@yandex.ru>
2022-02-23 13:45:41 +0300

    Initial Commit

---
+++ GitUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitUser
+//
+//  Created by Mikhail Kolkov on 23.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
